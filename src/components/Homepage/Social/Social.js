aa4588d766928d604b9d4da198e4cf93cdd4be7c
FrancoMRuiz <x.francoruiz@gmail.com>
2021-11-03 01:28:47 -0300

    add Fontawesome

--- src/components/Homepage/Social/Social.js
+++ src/components/Homepage/Social/Social.js
@@ -1,10 +1,27 @@
 import React from "react";
 import "./styles.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+
+const Linkedin = () => <FontAwesomeIcon icon={faLinkedin} />;
+const Github = () => <FontAwesomeIcon icon={faGithub} />;
+const Twitter = () => <FontAwesomeIcon icon={faTwitter} />;
+
 export const Social = ({ color, ...props }) => (
   <div className={`social__container`} {...props}>
     <div className={`social__icons__container`}>
-        
+      <a href={"https://www.linkedin.com/in/francomruiz/"}>
+        <Linkedin />
+      </a>
+      <a href={"https://github.com/fmruiz"}>
+        <Github />
+      </a>
+      <a href={"https://twitter.com/francoRuiz_"}>
+        <Twitter />
+      </a>
     </div>
 
     <div className={`social__mail__container`}>
