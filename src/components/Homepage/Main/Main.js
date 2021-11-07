17d44f494ccd343f418ffef27985ce1b341190fc
FrancoMRuiz <x.francoruiz@gmail.com>
2021-11-07 19:28:18 -0300

    add typewriter

--- src/components/Homepage/Main/Main.js
+++ src/components/Homepage/Main/Main.js
@@ -1,14 +1,37 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 import "./styles.css";
 
-export const Main = ({ color, ...props }) => (
-  <div className={`main__container`} {...props}>
-    <div className={`main__first ${color}`}>
-      <h1>
-        I'm <span>Franco Ruiz</span>
-      </h1>
-      <h3>Front-End Engineer</h3>
+export const Main = ({ color, ...props }) => {
+  return (
+    <div className={`main__container`} {...props}>
+      <div className={`main__first ${color}`}>
+        <div className={"main__text"}>
+          <span>I'm</span>
+          <h1 className={"h1__main"}>
+            <Typewriter
+              options={{
+                loop: true,
+                delay: 220
+              }}
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Franco Ruiz")
+                  .callFunction(() => {
+                    console.log("String typed out!");
+                  })
+                  .callFunction(() => {
+                    console.log("All strings were deleted");
+                  })
+                  .pauseFor(10000)
+                  .start();
+              }}
+            />
+          </h1>
+        </div>
+        <h3>Frontend Engineer</h3>
+      </div>
+      <div className={"main__second"}></div>
     </div>
-    <div className={"main__second"}></div>
-  </div>
-);
+  );
+};
